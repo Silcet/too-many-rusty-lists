5c96c927a578fdaacfcc6dd8e334605542dafb44
Silcet <camorga1@gmail.com>
2021-05-17 15:54:53 +0200

    Started fifth

---
+++ src/fifth.rs
@@ -0,0 +1,42 @@
+use std::mem;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_tail = Box::new(Node {
+            elem: elem,
+            next: None,
+        });
+
+        let new_tail = match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.next = Some(new_tail);
+                old_tail.next.as_deref_mut()
+            }
+            None => {
+                self.head = Some(new_tail);
+                self.head.as_deref_mut()
+            }
+        };
+
+        self.tail = new_tail;
+    }
+}
